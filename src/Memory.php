9ee537139fa553ba9f8f7e07036b26c9b0495699
Darío Rivera <fermius.us@gmail.com>
2021-07-20 15:42:52 -0500

    feat: create lib scaffolding

---
+++ src/Memory.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Pleets\MemoryTools;
+
+class Memory
+{
+
+}
---
+++ tests/Unit/MemoryTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Pleets\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use Pleets\MemoryTools\Memory;
+
+class MemoryTest extends TestCase
+{
+    /** @test */
+    public function itDoSomething()
+    {
+        $memory = new Memory();
+
+        $this->assertTrue(true);
+    }
+}
